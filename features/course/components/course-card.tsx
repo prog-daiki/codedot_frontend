a8e4bf167e8f239d2bbe90cf6b56b79884a8cecc
prog-daiki <t.daiki0913@icloud.com>
2024-10-08 11:34:20 +0900

    講座リストの作成

---
+++ features/course/components/course-card.tsx
@@ -0,0 +1,57 @@
+import { formatPrice } from "@/lib/format-price";
+import { BookOpen } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface CourseCardProps {
+  id: string;
+  title: string;
+  imageUrl: string;
+  chaptersLength: number;
+  price: number;
+  category: string;
+  purchased: boolean;
+}
+
+export const CourseCard = ({
+  id,
+  title,
+  imageUrl,
+  chaptersLength,
+  price,
+  category,
+  purchased,
+}: CourseCardProps) => {
+  return (
+    <Link href={`/courses/${id}`}>
+      <div className="group h-full overflow-hidden rounded-lg border p-3 hover:shadow-2xl shadow-lg transition duration-300 ease-in-out">
+        <div className="relative aspect-video w-full overflow-hidden rounded-md">
+          <Image
+            alt={title}
+            className="object-cover"
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            src={imageUrl}
+          />
+        </div>
+        <div className="flex flex-col pt-2">
+          <div className="line-clamp-2 text-lg font-bold transition group-hover:text-sky-700 md:text-base">
+            {title}
+          </div>
+          <p className="text-xs text-muted-foreground">{category}</p>
+          <div className="my-1 flex items-center gap-x-2 text-sm md:text-xs">
+            <div className="flex items-center gap-x-1 text-slate-500">
+              <BookOpen className="h-4 w-4 text-slate-500 bg-sky-100 rounded-full p-0.5" />
+              <span>
+                {chaptersLength} {chaptersLength > 1 ? "chapters" : "chapter"}
+              </span>
+            </div>
+          </div>
+          <p className="text-md font-semibold text-slate-700 md:text-sm">
+            {purchased ? "購入済み" : formatPrice(price)}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+};
---
+++ features/course/components/course-list.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { useGetPublishCourses } from "../api/use get-publish-courses";
+import { CourseCard } from "./course-card";
+
+interface CourseListProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+const SKELETON_COUNT = 8;
+
+export const CourseList = ({ searchParams }: CourseListProps) => {
+  const coursesQuery = useGetPublishCourses({
+    title: searchParams.title,
+    categoryId: searchParams.categoryId,
+  });
+  const courses = coursesQuery.data || [];
+
+  if (coursesQuery.isLoading) {
+    return (
+      <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4">
+        {Array.from({ length: SKELETON_COUNT }).map((_, index) => (
+          <Skeleton key={index} className="h-64 w-full" />
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4">
+        {courses.map((course) => (
+          <CourseCard
+            category={course.category!.name}
+            chaptersLength={course.chapters.length}
+            id={course.course.id}
+            imageUrl={course.course.imageUrl!}
+            key={course.course.id}
+            price={course.course.price!}
+            title={course.course.title}
+            purchased={course.purchased}
+          />
+        ))}
+      </div>
+      {courses.length === 0 && (
+        <div className="mt-10 text-center text-sm text-muted-foreground">
+          講座が見つかりませんでした
+        </div>
+      )}
+    </div>
+  );
+};
