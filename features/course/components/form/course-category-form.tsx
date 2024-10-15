322346337557acf7ad491977539cad6d11f3e5b4
prog-daiki <t-daiki0913@icloud.com>
2024-10-15 14:42:49 +0900

    不要なインポートの削除

--- features/course/components/form/course-category-form.tsx
+++ features/course/components/form/course-category-form.tsx
@@ -1,5 +1,4 @@
 import { z } from "zod";
-import { useUpdateCourseDescription } from "../../api/use-update-course-description";
 import { useState } from "react";
 import { useQueryClient } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
@@ -13,7 +12,6 @@ import {
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Pencil } from "lucide-react";
-import { Textarea } from "@/components/ui/textarea";
 import { useUpdateCourseCategory } from "../../api/use-update-course-category";
 import { cn } from "@/lib/utils";
 import { useGetCategories } from "@/features/category/api/use-get-categories";
