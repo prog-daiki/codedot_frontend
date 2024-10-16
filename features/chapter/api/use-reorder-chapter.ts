d6a34adedfe3ecc1e21ece0fee845e425b9849c6
prog-daiki <t.daiki0913@icloud.com>
2024-10-17 00:43:14 +0900

    並び替えAPI連携部分のリファクタリング

--- features/chapter/api/use-reorder-chapter.ts
+++ features/chapter/api/use-reorder-chapter.ts
@@ -1,16 +1,16 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useAuth } from "@clerk/nextjs";
 import { toast } from "sonner";
-import { Chapter } from "../types/chapter";
 
-type RequestType = { id: string; position: number }[];
-type ResponseType = Chapter;
+type RequestType = {
+  list: { id: string; position: number }[];
+};
 
 export const useReorderChapter = (courseId: string) => {
   const queryClient = useQueryClient();
   const { getToken } = useAuth();
-  const mutation = useMutation<ResponseType, Error, RequestType>({
-    mutationFn: async (json) => {
+  const mutation = useMutation<ResponseType, Error, RequestType["list"]>({
+    mutationFn: async (chapters) => {
       const token = await getToken();
       if (!token) {
         throw new Error("認証トークンの取得に失敗しました");
@@ -19,7 +19,7 @@ export const useReorderChapter = (courseId: string) => {
         `${process.env.NEXT_PUBLIC_API_URL}/courses/${courseId}/chapters/reorder`,
         {
           method: "PUT",
-          body: JSON.stringify(json),
+          body: JSON.stringify({ list: chapters }),
           headers: {
             Authorization: `Bearer ${token}`,
             "Content-Type": "application/json",
@@ -31,7 +31,6 @@ export const useReorderChapter = (courseId: string) => {
     onSuccess: (updatedChapters) => {
       toast.success("チャプターの順番を更新しました");
       queryClient.invalidateQueries({ queryKey: ["chapters", courseId] });
-      queryClient.setQueryData(["chapters", courseId], updatedChapters);
     },
     onError: (error) => {
       toast.error("チャプターの順番の更新に失敗しました");
