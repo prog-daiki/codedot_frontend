c081a8492a2ebd9fb6db2cb24284106de63d8387
prog-daiki <t.daiki0913@icloud.com>
2024-10-17 00:42:34 +0900

    dashboardpageの作成

---
+++ app/(main)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+const DashboardPage = () => {
+  return (
+    <div className="w-full">
+      <div className="max-w-6xl mx-auto px-6 py-12">
+        <h2 className="text-2xl font-bold">購入済み講座</h2>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
