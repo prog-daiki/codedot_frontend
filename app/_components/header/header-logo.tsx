e6c6873836af5e9c9639bcdc54820e54afb6a669
prog-daiki <t.daiki0913@icloud.com>
2024-10-24 11:05:39 +0900

    コンソールの警告の解消

--- app/_components/header/header-logo.tsx
+++ app/_components/header/header-logo.tsx
@@ -4,7 +4,14 @@ import Link from "next/link";
 export const HeaderLogo = () => {
   return (
     <Link href="/courses">
-      <Image src="/images/logo.png" alt="logo" width={60} height={60} />
+      <Image
+        src="/images/logo.png"
+        alt="logo"
+        width={60}
+        height={60}
+        style={{ width: "auto", height: "auto" }}
+        priority
+      />
     </Link>
   );
 };
