10c81b94af91e2197c3bb81017f2054b1eb0c94f
prog-daiki <t.daiki0913@icloud.com>
2024-10-24 17:00:04 +0900

    フッターのSP版の修正

--- app/_components/footer/footer.tsx
+++ app/_components/footer/footer.tsx
@@ -9,7 +9,7 @@ const footerLinks = [
 export const Footer = () => {
   return (
     <footer className="w-full border-t py-4 px-6 h-20">
-      <div className="w-full max-w-[1400px] mx-auto flex items-center h-full justify-between">
+      <div className="w-full max-w-[1400px] mx-auto flex flex-col md:flex-row gap-2 items-center h-full justify-between">
         <CopyRight />
         <FooterLinks />
       </div>
